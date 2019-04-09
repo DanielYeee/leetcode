e502df799338d3793ec71cc4f16e0d93547d736e
DanielYeee <1843126638@qq.com>
2019-04-09 21:48:33 +0800

    # WARNING: head commit changed in the meantime
    leetcode TwoNums

--- Leetcode/src/leetcode/TwoNums.java
+++ Leetcode/src/leetcode/TwoNums.java
@@ -1,17 +1,23 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * @version 1.0
  * @author DanielYeee
+ * 传递一个目标值，判断数组中是否有两数之和等于该目标值，有则打印位置和值，没有则抛出异常
  *
  */
 
 public class TwoNums {
 	public static void main(String[] args) {
 		int[] arr = {1,2,3,4,5,6,7,8,9,10};
-		String lcAndValue = leetcode2TwoNum(arr,6);
+		String lcAndValue = twoSum2(arr,5);
 		System.out.println(lcAndValue);
-		}
+	}
+
 		 
 
 	/**
@@ -26,10 +32,52 @@ public class TwoNums {
 			for(int i = 0; i < arr.length;i++) {
 				for(int j = i + 1;j < arr.length;j++) {
 					if(arr[j] == (target - arr[i])) {
-						return new String("location:" + i + "  " + j + ";value:" + arr[i] + " " + arr[j] );
+						return new String("location:" + i + "  " + j + "\nvalue:" + arr[i] + " " + arr[j] );
 					}
 				}
 			}
 			throw new IllegalArgumentException("No two sum solution");
 	}
+	
+	
+	/**
+	 * @param nums
+	 * @param target
+	 * @return String
+	 * 同时使用图和数组进行判断
+	 */
+	 public static String twoSum(int[] nums,int target) {
+		 	Map<Integer, Integer> map = new HashMap<>();
+		 	for(int i = 0;i < nums.length;i++) { 
+		 		map.put(nums[i],i);
+		 	}
+		 	for(int i = 0;i < nums.length;i++) {
+		 		int complement = target - nums[i];
+		 		if(map.containsKey(complement) && (map.get(complement) != i)) {	//判断值差存在和值不重复
+		 			return new String("location:" + i + "  " + map.get(complement) + "\nvalue:" + nums[i]  + " " + complement);
+		 		}
+		 	}
+		 	throw new IllegalArgumentException("No two sum solution");
+		 
+	 }
+	 
+	 /**
+	  * 
+	  * @param nums
+	  * @param target
+	  * @return String
+	  */
+	 public static String twoSum2(int[] nums,int target) {
+		 Map<Integer, Integer> map = new HashMap<>();
+		 for(int i = 0;i < nums.length;i++) {
+			 int complement = target - nums[i];
+			 if(map.containsKey(complement)) {
+				 return new String("locatin:" + map.get(complement) + " " + i + "\nvalue:" + complement + " " + nums[i]);
+			 }
+			 map.put(nums[i], i);
+		 }
+		 throw new IllegalArgumentException("No two sum solution");
+	 }
+	
+	
 }
